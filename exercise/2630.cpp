886f59fa8be153311a106b2d6abe26b37f5c0704
currentnine <wjdgusrn951@naver.com>
2025-05-01 18:32:51 +0900

    commit 0501

--- exercise/2630.cpp
+++ exercise/2630.cpp
@@ -4,11 +4,9 @@
 using namespace std;
 
 
-
-void div()
-{
-
-}
+void div(int n, int x, int y);
+int n, arr[128][128];
+int white, blue;
 
 int main()
 {
@@ -18,9 +16,29 @@ int main()
     {
         for(int j = 0; j<n;j++)
         {
-            
+            cin >> arr[i][j];
         }
     }
-    
+    div(n, 0 ,0);
+    cout << white << "\n" << blue;
+}
 
+
+void div(int n, int x, int y)
+{
+    for(int i =x; i<x+n; i++)
+    {
+        for(int j = y; j<y+n; j++)
+        {
+            if(!arr[i][j] == arr[x][y]){
+                div(n/2, x,y);
+                div(n/2, x+n/2,y);
+                div(n/2, x,y+n/2);
+                div(n/2, x+n/2,y+n/2);
+                return;
+            }
+        }
+    }
+    if(arr[x][y]) blue++;
+    else white++;
 }
