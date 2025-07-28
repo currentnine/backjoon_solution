a15a2ac72f5401ccd2c009ff642a433fefb78dc5
currentnine <wjdgusrn951@naver.com>
2025-07-28 09:29:47 +0900

    commit 0728

--- dijkstra/11779.cpp
+++ dijkstra/11779.cpp
@@ -8,16 +8,17 @@ using namespace std;
 #define X first
 #define Y second
 
-int v,e,st;
+int v,e,st,en;
 
-vector<pair<int,int>> adj[20005];
+vector<pair<int,int>> adj[1005];
 const int INF = 1e9+10;
-int d[20005];
+int d[1005];
+int pre[1005];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> v >> e >> st;
+    cin >> v >> e;
     fill(d,d+v+1, INF);
 
     while(e--){
@@ -25,6 +26,7 @@ int main(){
         cin >> u >> v >> w;
         adj[u].push_back({w,v});
     }
+    cin >> st >> en;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
 
     d[st] = 0;
@@ -36,12 +38,25 @@ int main(){
             if(d[nxt.Y] <= d[cur.Y] + nxt.X) continue;
             d[nxt.Y] = d[cur.Y] + nxt.X;
             pq.push({d[nxt.Y], nxt.Y});
+            pre[nxt.Y] = cur.Y;
         }
     }
 
-    for(int i = 1; i<= v; i++){
-        if(d[i] == INF) cout << "INF" << "\n";
-        else cout << d[i] << "\n";
+    cout << d[en] << "\n";
+    vector<int> path;
+    int cur = en;
+    while(cur != st){
+        path.push_back(cur);
+        cur = pre[cur];
     }
+    path.push_back(cur);
+    reverse(path.begin(), path.end());
+    cout << path.size() << "\n";
+    for(auto x: path) cout << x << " ";
+
+    // for(int i = 1; i<= v; i++){
+    //     if(d[i] == INF) cout << "INF" << "\n";
+    //     else cout << d[i] << "\n";
+    // }
 
 }
